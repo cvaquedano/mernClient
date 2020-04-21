e17b3e7bc653bf388164098e69ee69b4b78c39df
Carlos Vaquedano <chvb2002@hotmail.com>
2020-04-20 19:05:23 -0600

    implement react-router-dom

---
+++ src/components/auth/NuevaCuenta.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+const NuevaCuenta = () => {
+    return(
+        <h1>NuevaCuenta</h1>
+    );
+}
+
+export default NuevaCuenta;
---
+++ src/components/proyectos/Proyectos.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+const Proyectos = () => {
+    return(
+        <h1>as</h1>
+    );
+}
+
+export default Proyectos;
